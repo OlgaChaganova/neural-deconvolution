Fix normalization for PSF and images

src/deconv/classic/wiener.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 from skimage import restoration
 
@@ -17,6 +19,9 @@ def wiener_gray(blurred_image: np.array, psf: np.array, **algo_params) -> np.arr
     np.array
         Restored one-channel image.
     """
+    if psf.sum() != 1:
+        psf /= psf.sum()
+        logging.warning('PSF has sum more than 1. Normed')
     restored = restoration.wiener(image=blurred_image, psf=psf, **algo_params)
     return restored / restored.max()
 
@@ -38,7 +43,7 @@ def wiener_rgb(blurred_image: np.array, psf: np.array, **algo_params) -> np.arra
     """
     rgb_restored = []
     for i in range(blurred_image.shape[-1]):
-        restored = restoration.wiener(blurred_image[:, :, i], psf, **algo_params)
-        rgb_restored.append(restored / restored.max())
+        # restored = restoration.wiener(blurred_image[:, :, i], psf, **algo_params)
+        rgb_restored.append(wiener_gray(blurred_image[:, :, i], psf, **algo_params))
     rgb_restored = np.stack(rgb_restored)
     return np.transpose(rgb_restored, (1, 2, 0))